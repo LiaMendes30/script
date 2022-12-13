b64f849086cb8a5d20336df7dd2f8ee6404e680e
LiaMendes30 <115584305+LiaMendes30@users.noreply.github.com>
2022-12-13 18:24:43 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let name: "Lya";
+console.log (name);
+let age: 19;
+console.log (age);
+
+let agex7 = 133;
+console.log (age7);
+let Friends = ["Samot", "Mariana", "Alice"];
+let numbers = ["1", "2", "3"];
+console.log (Friends [0])
+
+let Friends = [
+    {
+        name: "Samot",
+        age: 19
+        isStudent: true
+    }
+    {
+        name: "Mariana",
+        age: 19
+        isStudent: true
+    }
+    {
+        name: "Jorge",
+        age: 29
+        isStudent: false
+    }
+]
+console.log (Friends.name)
+
+
